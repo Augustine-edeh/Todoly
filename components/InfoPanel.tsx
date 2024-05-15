43e21c70d3a7632e9d220b83b5ca1825fcf5a3ec
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-15 18:39:18 +0100

    modify InfoPanel styles

--- components/InfoPanel.tsx
+++ components/InfoPanel.tsx
@@ -2,7 +2,7 @@ import Filter from "./Filter";
 
 const InfoPanel = ({ todoList }) => {
   return (
-    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-2 border-t-[0.1px] border-very-dark-grayish-blue-L">
+    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-5 border-t-[0.1px] border-very-dark-grayish-blue-L rounded-b">
       <div>
         {todoList.length === 1
           ? "1 item left"
