40c395af8ceb34cc3e9fd1fe60afb8b4184b86fe
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-15 18:35:40 +0100

    chore: add border for InfoPanel

--- components/InfoPanel.tsx
+++ components/InfoPanel.tsx
@@ -2,7 +2,7 @@ import Filter from "./Filter";
 
 const InfoPanel = ({ todoList }) => {
   return (
-    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-2">
+    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-2 border-t-[0.1px] border-very-dark-grayish-blue-L">
       <div>
         {todoList.length === 1
           ? "1 item left"
