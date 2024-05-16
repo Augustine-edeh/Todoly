c3adfb0b5a44e521e8f8633e482b2e41c94ee651
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-16 19:31:30 +0100

    modify state variable name

--- components/InfoPanel.tsx
+++ components/InfoPanel.tsx
@@ -2,14 +2,14 @@ import { useCounterStore } from "@/store/store";
 import Filter from "./Filter";
 
 const InfoPanel = () => {
-  const todoList = useCounterStore((state) => state.todoArray);
+  const todoArray = useCounterStore((state) => state.todoArray);
   return (
     <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-5 border-t-[0.1px] border-very-dark-grayish-blue-L rounded-b">
       <div>
-        {todoList.length === 1
+        {todoArray.length === 1
           ? "1 item left"
-          : todoList.length > 1
-          ? `${todoList.length} items left`
+          : todoArray.length > 1
+          ? `${todoArray.length} items left`
           : "No item left"}
       </div>
 
