7c41a30e0197204b359b628e6676d34a3e0f2d06
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-17 16:57:10 +0100

    chore: add active & hover states for Filter component

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -2,9 +2,24 @@ const Filter = ({ className }) => {
   return (
     <div className={`${className} text-dark-grayish-blue-D`}>
       <div className="flex justify-center gap-x-6">
-        <button type="button">All</button>
-        <button type="button">Active</button>
-        <button type="button">Completed</button>
+        <button
+          type="button"
+          className="cursor-pointer focus:text-bright-blue hover:text-light-grayish-blue-D_"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="cursor-pointer focus:text-bright-blue hover:text-light-grayish-blue-D_"
+        >
+          Active
+        </button>
+        <button
+          type="button"
+          className="cursor-pointer focus:text-bright-blue hover:text-light-grayish-blue-D_"
+        >
+          Completed
+        </button>
       </div>
     </div>
   );
