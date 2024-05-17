eb30475620e6d1be1fabac1f9085cf883b8bfe2b
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-17 17:04:35 +0100

    chore(InfoPanel): add element active state

--- components/InfoPanel.tsx
+++ components/InfoPanel.tsx
@@ -4,7 +4,7 @@ import Filter from "./Filter";
 const InfoPanel = () => {
   const todoArray = useTodoStore((state) => state.todoArray);
   return (
-    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-5 border-t-[0.1px] border-very-dark-grayish-blue-L rounded-b">
+    <div className="flex justify-between mt-auto text-dark-grayish-blue-D bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-5 border-t-[0.1px] border-very-dark-grayish-blue-L rounded-b select-none">
       <div>
         {todoArray.length === 1
           ? "1 item left"
@@ -14,7 +14,10 @@ const InfoPanel = () => {
       </div>
 
       <Filter className={"hidden md:block"} />
-      <div>Clear Completed</div>
+
+      <div className="cursor-pointer hover:text-light-grayish-blue-D_">
+        Clear Completed
+      </div>
     </div>
   );
 };
