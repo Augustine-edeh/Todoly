d5ed404255911fdfc8ac3237283d865c12d1d8b0
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-19 07:28:50 +0100

    chore: set default filter to `All` & add active-state styles for the `All` filter

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -6,21 +6,16 @@ const Filter = ({ className }) => {
   // const { filter, setFilter, clearCompleted } = useTodoStore();
   const filter = useTodoStore((state) => state.filter);
   const setFilter = useTodoStore((state) => state.setFilter);
-  const clearCompleted = useTodoStore((state) => state.clearCompleted);
-
-  // <div>
-  //   <button onClick={() => setFilter('all')} disabled={filter === 'all'}>All</button>
-  //   <button onClick={() => setFilter('active')} disabled={filter === 'active'}>Active</button>
-  //   <button onClick={() => setFilter('completed')} disabled={filter === 'completed'}>Completed</button>
-  //   <button onClick={clearCompleted}>Clear Completed</button>
-  // </div>
+  // const clearCompleted = useTodoStore((state) => state.clearCompleted);
 
   return (
     <div className={`${className} text-dark-grayish-blue-D select-none`}>
       <div className="flex justify-center gap-x-6">
         <button
-          type="button"
-          className="cursor-pointer focus:text-bright-blue hover:text-light-grayish-blue-D_"
+          type={"button"}
+          className={`cursor-pointer focus:text-bright-blue hover:text-light-grayish-blue-D_ ${
+            filter === "all" ? "text-bright-blue" : ""
+          }`}
           onClick={() => setFilter("all")}
           // disabled={filter === "all"}
         >
