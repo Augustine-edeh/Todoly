b65e789d689409063f899c326118023e1d395545
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-19 15:12:38 +0100

    chore: modify project metadata information

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo App",
+  title: "Todoly",
   description:
-    "Effortlessly manage tasks, set priorities, and stay organized with intuitive features designed to boost your efficiency. Try it now",
+    "Effortlessly manage tasks, set priorities, and stay organized with intuitive features designed to boost your efficiency. Try Todoly now",
 };
 
 export default function RootLayout({
