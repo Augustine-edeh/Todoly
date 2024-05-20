9d37495718877fd25182edd923bb618c2f327960
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-20 22:07:13 +0100

    bugFix: resolve darkmode glitch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkmode: "class",
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
