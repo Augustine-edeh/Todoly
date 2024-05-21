b216914a03e37782591731d2999345ea27e678ee
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-21 22:57:34 +0100

    chore: modify text colour for Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -18,7 +18,7 @@ const Input = () => {
     >
       <input
         type="text"
-        className="relative w-full rounded-md py-3 px-5 outline-none bg-very-light-gray-Lfsdl dark:bg-very-dark-desaturated-blue-D text-red-300 text-lg border-0 peer"
+        className="relative w-full rounded-md py-3 px-5 outline-none bg-very-light-gray-Lfsdl dark:bg-very-dark-desaturated-blue-D text-gray-600 dark:text-white/70 text-lg border-0 peer"
         placeholder=" Create a new todo..."
         onChange={(e) => setNewTaskk(e.target.value)}
         value={newTaskk}
