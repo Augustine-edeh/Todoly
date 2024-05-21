981c88794d422601d7042383c7124fde31ee1b06
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-21 23:12:42 +0100

    chore: modify Input styles

--- components/Input.tsx
+++ components/Input.tsx
@@ -14,18 +14,18 @@ const Input = () => {
       onSubmit={(e) => {
         e.preventDefault(), updateTodoArray();
       }}
-      className="relative flex items-center bg-very-light-gray-Lfsdl dark:bg-very-dark-desaturated-blue-D rounded-md shadow-xl"
+      className="relative flex items-center bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D rounded-md shadow-xl"
     >
       <input
         type="text"
-        className="relative w-full rounded-md py-3 px-5 outline-none bg-very-light-gray-Lfsdl dark:bg-very-dark-desaturated-blue-D text-gray-600 dark:text-white/70 text-lg border-0 peer"
+        className="relative w-full rounded-md py-3 px-5 outline-none bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D text-gray-600 dark:text-white/70 text-lg border-0 peer"
         placeholder=" Create a new todo..."
         onChange={(e) => setNewTaskk(e.target.value)}
         value={newTaskk}
         id="input"
       />
 
-      <div className="absolute right-3 h-8 w-8 bg-very-dark-desaturated-blue-D p-1 peer-focus:block hidden">
+      <div className="absolute right-3 h-8 w-8 bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-1 peer-focus:block hidden">
         <label
           htmlFor="input"
           className="block h-full w-full border-[0.1px] border-very-dark-grayish-blue-L rounded-full cursor-pointer"
