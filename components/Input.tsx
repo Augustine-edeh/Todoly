513fc149d2d35f383195ca6fc8e86259de3d86d7
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-23 05:23:41 +0100

    chore: add submit button

--- components/Input.tsx
+++ components/Input.tsx
@@ -25,13 +25,26 @@ const Input = () => {
         id="input"
       />
 
-      <div className="absolute right-3 h-8 w-8 bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-1 peer-focus:block hidden">
-        <label
-          htmlFor="input"
-          className="block h-full w-full border-[0.1px] border-very-dark-grayish-blue-L rounded-full cursor-pointer"
-          // onClick={submitHandler}
-        />
-      </div>
+      <button
+        onClick={updateTodoArray}
+        type="button"
+        className="flex place-items-center absolute right-2 h-10 w-10 bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-1 rounded-full border border-very-dark-grayish-blue-L rounded-ful"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          viewBox="0 0 36 36"
+          className="h-8 w-8"
+        >
+          <path
+            fill="currentColor"
+            d="M30 17H19V6a1 1 0 1 0-2 0v11H6a1 1 0 0 0-1 1a.91.91 0 0 0 1 .94h11V30a1 1 0 1 0 2 0V19h11a1 1 0 0 0 1-1a1 1 0 0 0-1-1"
+            className="clr-i-outline clr-i-outline-path-1"
+          />
+          <path fill="none" d="M0 0h36v36H0z" />
+        </svg>
+      </button>
     </form>
   );
 };
