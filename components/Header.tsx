0956832e0d7eb3f04f0b01b2123d41fb05c056d5
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-05-23 10:24:55 +0100

    chore: add letter-spacing for project header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "./ThemeSwitch";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center text-white">
+    <header className="flex justify-between items-center text-white tracking-[0.25rem]">
       <h1 className="font-bold text-3xl">TODOLY</h1>
       <ThemeSwitch />
     </header>
