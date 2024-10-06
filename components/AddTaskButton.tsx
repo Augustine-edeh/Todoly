47a8dc97e1736429352909372917dd3ce8dbde9b
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-10-06 23:34:55 +0100

    refactor: refactor 'add task' button into its own component for better code organization

---
+++ components/AddTaskButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useTodoStore } from "@/store/todoStore";
+
+const AddTaskButton = () => {
+  const updateTodoArray = useTodoStore((state) => state.updateTodoArray);
+
+  return (
+    <button
+      onClick={updateTodoArray}
+      type="button"
+      className="flex place-items-center absolute right-2 h-10 w-10 bg-very-light-gray-L dark:bg-very-dark-desaturated-blue-D p-1 rounded-full border border-very-dark-grayish-blue-L rounded-ful"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        className="h-8 w-8"
+      >
+        <path
+          fill="currentColor"
+          d="M30 17H19V6a1 1 0 1 0-2 0v11H6a1 1 0 0 0-1 1a.91.91 0 0 0 1 .94h11V30a1 1 0 1 0 2 0V19h11a1 1 0 0 0 1-1a1 1 0 0 0-1-1"
+          className="clr-i-outline clr-i-outline-path-1"
+        />
+        <path fill="none" d="M0 0h36v36H0z" />
+      </svg>
+    </button>
+  );
+};
+
+export default AddTaskButton;
